76fb74b46bf6668e1fdc17783f55d754e672bb93
niwanowa <ky14311@tomakomai.kosen-ac.jp>
2019-01-28 09:42:17 +0900

    プログラミング王決定戦予選 c問題

--- 全国統一プログラミング王決定戦予選/c.py
+++ 全国統一プログラミング王決定戦予選/c.py
@@ -1,21 +1,35 @@
-from operator import itemgetter
 n = int(input())
 ab = []
-a = []
 b = []
-flag = 0
 for _ in range(n):
     tmp = list(map(int, input().split()))
-    ab.append(tmp)
-while True:
-    if flag == 0:
-        flag = 1
-        ab.sort(reverse=True, key=itemgetter(0))
-        a.append(ab.pop(0)[0])
-    else:
-        flag = 0
-        ab.sort(reverse=True, key=itemgetter(1))
-        b.append(ab.pop(0)[1])
-    if len(ab) == 0:
-        break
-print(sum(a) - sum(b))
+    b.append(tmp[1])
+    ab.append(sum(tmp))
+ab.sort(reverse=True)
+print(sum(ab[::2]) - sum(b))
+
+
+
+
+# 記念
+# from operator import itemgetter
+# n = int(input())
+# ab = []
+# a = []
+# b = []
+# flag = 0
+# for _ in range(n):
+#     tmp = list(map(int, input().split()))
+#     ab.append(tmp)
+# while True:
+#     if flag == 0:
+#         flag = 1
+#         ab.sort(reverse=True, key=itemgetter(0))
+#         a.append(ab.pop(0)[0])
+#     else:
+#         flag = 0
+#         ab.sort(reverse=True, key=itemgetter(1))
+#         b.append(ab.pop(0)[1])
+#     if len(ab) == 0:
+#         break
+# print(sum(a) - sum(b))
